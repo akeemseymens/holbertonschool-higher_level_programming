c50ca61c9580d075804e2660e0f2f98c1328e6b2
aseymens <akeem.seymens>
2019-09-10 21:37:11 +0000

    added file.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(str):
+    for ltr in str
+        if ord(ltr) >= ord('a') and ord(ltr) <= ord('z'):
+            case = 32
+        else:
+            case = 0
+        print('{:c}'.format(ord(ltr) - case), end="")
+    print()
