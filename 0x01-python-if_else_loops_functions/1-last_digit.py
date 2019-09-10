3d4f94d6e96409db0b4a5f4d782bb4e3722eeaaf
aseymens <akeem.seymens>
2019-09-10 20:52:36 +0000

    rearrange algo for negative numbers.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_num = number % 10
-str1 = "and is greater than 5"
-str2 = "and is 0"
-str3 = "and is less than 6 and not 0"
-print("Last digit of", end=" ")
+last_num = abs(number) % 10
+if number < 0:
+    last_num = -last_num
+print("Last digit of {} is {} and is".format(number, last_num), end=" ")
 if last_num > 5:
-    print('{} is {} {}'.format(number, last_num, str1))
+    print("greater than 5")
 elif last_num == 0:
-    print('{:d} is {} {}'.format(number, last_num, str2))
-elif number < 0:
-    print('{:d} is {} {}'.format(number, (last_num * -1), str3))
+    print("0")
 else:
-    print('{:d} is {} {}'.format(number, last_num, str3))
+    print("less than 6 and not 0")
