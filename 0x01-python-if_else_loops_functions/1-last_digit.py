f126f48109b6e57319b0726aa8c55f4caef2d25f
aseymens <akeem.seymens>
2019-09-10 19:47:36 +0000

    fixed algorithm

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,14 @@ number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
 str0 = "Last digit of"
 str1 = "and is greater than 5"
-str2 = "and is less than 6 and not 0"
-str3 = "and is 0"
+str2 = "and is 0"
+str3 = "and is less than 6 and not 0"
+
 if last_num > 5:
-    print('{0} {1:d} is {2:d} {3}'.format(str0, number, last_num, str1))
-elif last_num < 6 and last_num > 0:
-    print('{0} {1:d} is {2:d} {3}'.format(str0, number, last_num, str2))
+    print('{} {:d} is {:d} {}'.format(str0, number, last_num, str1))
+elif last_num == 0:
+    print('{} {:d} is {:d} {}'.format(str0, number, last_num, str2))
+elif number < 0:
+    print('{} {:d} is {:d} {}'.format(str0, number, (last_num * -1), str3))
 else:
-    print('{0} {1:d} is {2:d} {3}'.format(str0, number, last_num, str3))
+    print('{} {:d} is {:d} {}'.format(str0, number, last_num, str3))
