65da922a5e46a1133ca093fb85d60cee1284e7e3
aseymens <akeem.seymens>
2019-09-10 19:33:59 +0000

    made pep compliant

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
+str0 = "Last digit of"
+str1 = "and is greater than 5"
+str2 = "and is less than 6 and not 0"
+str3 = "and is 0"
 if last_num > 5:
-    print('Last digit of {0:d} is {1:d} and is greater than 5'.format(number, last_num))
+    print('{0} {1:d} is {2:d} {3}'.format(str0, number, last_num, str1))
 elif last_num < 6 and last_num > 0:
-    print('Last digit of {0:d} is {1:d} and is less than 6 and not 0'.format(number, last_num))
+    print('{0} {1:d} is {2:d} {3}'.format(str0, number, last_num, str2))
 else:
-    print('Last digit of {0:d} is {1:d} and is 0'.format(number, last_num))
+    print('{0} {1:d} is {2:d} {3}'.format(str0, number, last_num, str3))
