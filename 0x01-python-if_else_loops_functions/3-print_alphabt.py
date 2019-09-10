1ba24bb5b9e183545d8251c3f3d3d4c4b0e96f9a
aseymens <akeem.seymens>
2019-09-10 21:10:26 +0000

    can't use cont.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for alphbt_lwr in range(ord('a'), ord('z')+1):
-    if alphbt_lwr == 'e' or alphbt_lwr == 'q':
-       continue
-    print("{:c}".format(alphbt_lwr), end="")
+       if alphbt_lwr != ord('q') and alphbt_lwr != ord('e'):
+           print("{:c}".format(alphbt_lwr), end="")
