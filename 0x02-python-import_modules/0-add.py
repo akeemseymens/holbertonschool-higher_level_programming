e57dbd1996ca8f5dd86dbb2a5e1552b386c8ddb1
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-12 18:29:26 +0000

    misspelled __name_ replaced with __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import add_0 import add
+from  add_0 import add
 
 def main():
     a = 1
     b = 2
     print('{} + {} = {}'.format(a, b, add(a,b)))
 
-if __name_ == "__main__":
+if __name__ == "__main__":
     main()
