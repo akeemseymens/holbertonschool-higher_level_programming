cf8dd896599c6ec28eeda1a17242b7730d9be490
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-18 06:25:14 +0000

    betty compliant

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,30 @@
 #include "lists.h"
 /**
- *is_Palindrome - checks to see if linked list is palindrome.
+ *is_Palindrome_helper - checks to see if linked list is palindrome.
  *@left: left-ward end list
  *@right: righ-ward end of list
  *
  *Return: 0 on success
  **/
 
-int is_Palin(listint_t **left, listint_t *right)
+int is_Palindrome_helper(listint_t **left, listint_t *right)
 {
 	int check;
 
 	if (right == NULL)
-		return 1;
-	check = is_Palindrome(left, right->next);
+		return (1);
+	check = is_Palindrome_helper(left, right->next);
 	if (check == 0)
 		return (0);
-        if (right->n == (*left)->n)
+	if (right->n == (*left)->n)
 	{
 		(*left) = (*left)->next;
 		return (1);
 	}
-	return 0;
+	return (0);
 }
 /**
- * is_palindrom_recurs - recursively calls the function is_Palindrome
+ * is_palindrome- recursively calls the function is_Palindrome
  * @head: head of node
  *Return: 0 on success
  **/
