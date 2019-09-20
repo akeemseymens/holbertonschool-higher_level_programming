d2aac8ced1d5795702d9a04b94751d5e15d351c1
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-20 05:41:06 +0000

    added file.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    total = 0
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    number = 0
+    dic = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1}
+    for char in reversed(roman_string):
+        if dic[char] < current:
+            number -= dic[char]
+        else:
+            number += dic[char]
+        total = dic[char]
+    return total
