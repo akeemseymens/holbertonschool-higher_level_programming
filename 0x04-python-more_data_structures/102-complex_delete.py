39a281abdf7cef7150e09a60c1944241862bc13c
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-20 06:44:06 +0000

    added file

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key in list(filter(lambda x: a_dictionary[x] == value, a_dictionary)):
+        a_dictionary.pop(k)
+    return a_dictionary
