625a312e416e07eca8111b13e2953ed580141658
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-01 02:59:49 +0000

    changed algo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    printed = 0
-    while printed < x:
-        try:
-            print(my_list[printed], end="")
+    try:
+        printed = 0
+        while printed < x:
+            print(my_list[printed], end='')
             printed += 1
-        except IndexError:
-            print()
-    print()
-
+        print()
+    except:
+        print()
     return printed
