ec90e49d1d2e0fa456733f1b6511e9b13f9078be
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-01 02:49:35 +0000

    pep8 formating

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    printed = 0;
+    printed = 0
     while printed < x:
         try:
             print(my_list[printed], end="")
-            printed +=1
+            printed += 1
         except IndexError:
             print()
     print()
