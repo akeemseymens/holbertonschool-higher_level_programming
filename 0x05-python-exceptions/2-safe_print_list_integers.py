9eb5acf88c6907837790078fd4fc1c606e1791ae
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-01 03:22:10 +0000

    changed made

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,5 @@
-#!usr/bin/python3
-def safe_print_list_integer(my_list=[], x=0):
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     num = 0
 
@@ -10,7 +10,7 @@ def safe_print_list_integer(my_list=[], x=0):
             printed -= 1
         finally:
             printed += 1
-            n += 1
+            num += 1
     print('')
 
     return printed
