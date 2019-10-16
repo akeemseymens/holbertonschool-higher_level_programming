bf7d91ae38592cde41cc4a949a94bc88a5e31ca5
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-16 07:11:39 +0000

    format change

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,7 +4,6 @@ This funciotn is to look at the attributes of the method of any object.
 
 """
 
-
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
