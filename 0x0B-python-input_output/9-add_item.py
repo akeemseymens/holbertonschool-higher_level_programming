dcffd9239fba5ac86f6fac4a314f99f6845c4626
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-17 03:04:29 +0000

    added file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,16 +4,20 @@
 """
 
 
-import sys
+from sys import argv
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 
-listn = []
+filename = "add_item.json"
+
+
 try:
-    listn = load_json("add_item.json")
+    listn = load_json(filename)
 except:
     listn = []
+
 elements = len(sys.argv)
-for numbers in range(1, elements):
-    listing.append(sys.argv[numbers])
-save_json(listn, "add_item.json")
+
+for nums in range(1, elements):
+    listing.append(sys.argv[nums])
+save_json(listn, filename)
