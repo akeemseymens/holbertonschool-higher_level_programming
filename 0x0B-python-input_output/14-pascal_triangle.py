6d6b625f0abd571275c7a334dc85894d86b8bb80
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-17 06:29:16 +0000

    fixed bugs

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -2,14 +2,16 @@
 """pascal triangle function"""
 
 
-def printPascal(n):
+def pascal_triangle(n):
+    """function of pascal"""
     arr = []
-    if n<= 0:
+    if n <= 0:
         return arr
     for line in range(1, n + 1):
-        C = 1;
+        C = 1
         pas = []
         for i in range(1, line + 1):
-            arr.append(str(C))
+            pas.append(str(C))
             C = int(C * (line - i) / i)
-       return pas
+        arr.append(pas)
+        return arr
