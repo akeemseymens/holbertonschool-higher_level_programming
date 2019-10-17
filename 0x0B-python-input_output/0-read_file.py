dd2bd0cdb98c1f441f69ad1b285ef36a576cf6c5
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-17 01:35:33 +0000

    added UTFD

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ function to read a file.
 def read_file(filename=""):
     """read a file."""
 
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end='')
