1dcca82af8b59b34134c9c8bc336edd715d734b8
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-17 02:58:30 +0000

    changed vars

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""
+
+"""
+
+
 import sys
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 
-listing = []
+listn = []
 try:
-    listing = load_json("add_item.json")
+    listn = load_json("add_item.json")
 except:
-    listing = []
+    listn = []
 elements = len(sys.argv)
 for numbers in range(1, elements):
     listing.append(sys.argv[numbers])
-save_json(listing, "add_item.json")
+save_json(listn, "add_item.json")
