f895208717337ca0cbec1d1013a05a7ff763e643
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-17 06:22:16 +0000

    added file

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""pascl triangle function"""
+
+
+def printPascal(n):
+    arr = []
+    if n<= 0:
+        return arr
+    for line in range(1, n + 1):
+        C = 1;
+        pas = []
+        for i in range(1, line + 1):
+            arr.append(str(C))
+            C = int(C * (line - i) / i)
+       return pas
