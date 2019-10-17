84942b518a0cddc91f44f0a6b542f9e04a077804
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-17 03:09:20 +0000

    added chmod

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-
-"""
-
-
 from sys import argv
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 
@@ -16,8 +12,7 @@ try:
 except FileNotFoundError:
     listn = []
 
-elements = len(sys.argv)
 
-for nums in range(1, elements):
+for nums in range(1, len(argv)):
     listing.append(sys.argv[nums])
 save_json(listn, filename)
