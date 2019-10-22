32a608c8d5d9b41fec21a2cea078d19fc4699b7e
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-22 21:44:33 +0000

    added comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """intialialized the base"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
