e20fdd0fa55c81218e3f8ae1e7abb406fe30fee5
akeemseymens <812@holbertonschool.com>
2019-11-26 21:44:24 -0800

    cpyed the wrong file.

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,9 @@
--- list all shows
-SELECT tg.name AS genre, COUNT(*) as number_of_shows FROM tv_genres tg INNER JOIN tv_show_genres tsg ON tg.id = tsg.genre_id GROUP BY genre ORDER BY number_of_shows DESC;
+-- list all dexter show
+SELECT tg.name
+FROM tv_genres tg
+INNER JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
+INNER JOIN tv_shows ts
+ON ts.id = tsg.show_id
+WHERE ts.title = "Dexter"
+ORDER BY tg.name;
