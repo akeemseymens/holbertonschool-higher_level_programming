7c463a689ddb7e1d1dd81799209be3aa4736470d
akeemseymens <812@holbertonschool.com>
2019-12-15 14:57:10 +0000

    port adjustment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3360, user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=33060, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY id"
