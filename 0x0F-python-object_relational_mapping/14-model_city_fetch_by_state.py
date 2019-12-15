e4dfa8c6fe6b72f134b232ca6e7b3211e60bcdca
akeemseymens <812@holbertonschool.com>
2019-12-15 19:00:07 +0000

    formated

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     session = Session()
     for city in session.query(City).order_by(City.id).all():
         print("{}: ({}) {}".format(city.state.name, city.id, city.name))
-    session.close()
