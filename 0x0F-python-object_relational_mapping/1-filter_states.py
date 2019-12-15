ac232a94564604d24fc7c17c5fb45f9621392988
akeemseymens <akeem.seymens@holbertonschool.com>
2019-12-15 13:43:48 +0000

    argv misspell

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,8 +3,8 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=arg[2], 
-                    db=arg[3], charset="utf8")
+db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=argv[2],
+                    db=argv[3], charset="utf8")
 if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
