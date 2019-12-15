5fd9384186b7a5c4647da08030ea7aa3bf66e741
akeemseymens <812@holbertonschool.com>
2019-12-15 15:11:13 +0000

    deleted syntax error

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
-                (sys.argv[4], ))
+                (argv[4], ))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
