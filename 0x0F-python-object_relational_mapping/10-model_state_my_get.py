56947ab38c8b00a5c534b35c2f82c33e2e88275d
akeemseymens <812@holbertonschool.com>
2019-12-15 17:47:54 +0000

    pep8 format

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,11 +12,10 @@ if __name__ == "__main__":
                 'mysql+mysqldb://{}:{}@localhost/{}'.
                 format(argv[1], argv[2], argv[3]),
                 pool_pre_ping=True)
-
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        state = session.query(State).filter(State.name == argv[4]).first()
+        state = session.query(State).filter(State.name == sys.argv[4]).first()
         if state:
                 print(state.id)
         else:
