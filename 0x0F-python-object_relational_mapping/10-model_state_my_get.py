4fe08096112c4b2eb7d05818670291162306a143
akeemseymens <812@holbertonschool.com>
2019-12-15 17:44:54 +0000

    syntax change.

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,9 +16,8 @@ if __name__ == "__main__":
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        state = session.query(State).filter(State.name == sys.argv[4]).first()
+        state = session.query(State).filter(State.name == argv[4]).first()
         if state:
                 print(state.id)
         else:
                 print("Not Found")
-        session.close()
