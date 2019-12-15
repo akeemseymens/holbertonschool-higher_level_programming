06401c1367115f6a08d29e5d5c6d2bfbb235006c
akeemseymens <812@holbertonschool.com>
2019-12-15 18:28:39 +0000

    added file

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+script that State object with the name passed as argument
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost/{}'.
+                format(argv[1], argv[2], argv[3]),
+                pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        state = session.query(State).filter(State.id == 2).first()
+        state.name = 'New Mexico'
+        session.commit()
+        session.close()
