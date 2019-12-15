b7cfe6c0705eeb90a953b698d7cc65da55863573
akeemseymens <akeem.seymens@holbertonschool.com>
2019-12-15 13:40:48 +0000

    added sys module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,16 @@
 """ lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
-from sys import argv
+import sys
 
-db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=argv[2], 
-                    db=argv[3], charset="utf8")
+
 if __name__ == "__main__":
-    cur = db.cursor()
-    cur.execute("SELECT * FROM state ORDERED BY id")
-    query_rows = db.fetchall()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
