808d01b35028bb89d40bbf666cbf7b5075e79333
akeemseymens <812@holbertonschool.com>
2019-12-15 10:52:03 -0800

    formated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ lists all states from the database hbtn_0e_0_usa
 """
-import MySQLdb
-from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=argv[2], 
-                    db=argv[3], charset="utf8")
+
 if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], 
+                    db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM state ORDERED BY id")
     query_rows = db.fetchall()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-
+""" Script that lists all states with a name starting with N (upper N) from the database"""
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=arg[2], 
-                    db=arg[3], charset="utf8")
-if __name__ == "__main__":
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    query_rows = db.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
 
+if __name__ == "__main__":
+
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=arg[2], 
+                        db=arg[3], charset="utf8")
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+        query_rows = db.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        db.close()
