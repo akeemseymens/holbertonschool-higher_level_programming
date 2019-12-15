0abe83e14eb0802b8fbf1d76839da9e69e835ceb
akeemseymens <812@holbertonschool.com>
2019-12-15 15:09:22 +0000

    syntax fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,14 +2,18 @@
 
 import MySQLdb
 from sys import argv
+"""
+displays all SQL values where name matches the last argument
+MySQL injections proof
+"""
 
-db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=arg[2], 
-                    db=arg[3], charset="utf8")
 if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
                 (sys.argv[4], ))
-    query_rows = db.fetchall()
+    query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
