5595529a6d4cf8c1cff5ea0e2046ecca3e916b12
akeemseymens <812@holbertonschool.com>
2019-12-15 14:35:32 +0000

    spelling

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 if __name__ == "__main__":
 
         db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=arg[2], db=arg[3], charset="utf8")
+                             passwd=argv[2], db=argv[3], charset="utf8")
         cur = db.cursor()
         cur.execute(
                 "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
