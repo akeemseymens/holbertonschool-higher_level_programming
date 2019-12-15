dd112a2958e06f302042ea0f25cc6db27d3077b2
akeemseymens <812@holbertonschool.com>
2019-12-15 11:02:00 -0800

    format issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa
+""" 
+lists all states from the database hbtn_0e_0_usa
 """
-<<<<<<< HEAD
-
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], 
-                    db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM state ORDERED BY id")
-    query_rows = db.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
-=======
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    db = MySQLdb.connect(host="localhost", port=3306, user= argv[1],
+                           passwd= argv[2], db= argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
-    conn.close()
->>>>>>> ac232a94564604d24fc7c17c5fb45f9621392988
+    db.close()
