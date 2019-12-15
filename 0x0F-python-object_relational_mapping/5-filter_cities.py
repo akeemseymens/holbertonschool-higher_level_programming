67de1a644d3151bcb80968a8ac75479164da6610
akeemseymens <812@holbertonschool.com>
2019-12-15 15:48:22 +0000

    pep8 issue

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" takes name of a state as an argument and lists all cities of that state,
-    SQL injection free!
+"""
+takes name of a state as an argument and lists all cities of that state,
+SQL injection proof
 """
 import MySQLdb
 from sys import argv
@@ -8,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("""SELECT cities.name FROM cities
     INNER JOIN states ON cities.state_id=states.id WHERE states.name=%s""",
