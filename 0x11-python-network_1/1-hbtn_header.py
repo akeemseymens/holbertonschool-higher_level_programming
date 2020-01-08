280e0d0218353811c87474c9dec8f84a5461d333
akeemseymens <812@holbertonschool.com>
2020-01-08 19:40:52 +0000

    pep8'ed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#value of the X-Request-Id variable found in the header of the response.
+'''  value of the X-Request-Id variable found in the header of the response.'''
+
+
 if __name__ == '__main__':
     import urllib.request
     from sys import argv
