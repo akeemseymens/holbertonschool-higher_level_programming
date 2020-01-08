0e9ae46d5ab3fca3b75a5c2f85db3a559ab8a7bb
akeemseymens <812@holbertonschool.com>
2020-01-08 19:38:16 +0000

    submit file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+#value of the X-Request-Id variable found in the header of the response.
+if __name__ == '__main__':
+    import urllib.request
+    from sys import argv
+    with urllib.request(urlopen(argv[1])) as response:
+        xreq_id = response.getheader('X-request-ID')
+    print(xreq_id)
