2e2aae4fdf08345fef95757340f04e69c902ae17
akeemseymens <812@holbertonschool.com>
2020-01-17 01:54:05 +0000

    fixed bug

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(r.json().get('id'))#!/usr/bin/python3
+    print(r.json().get('id'))
