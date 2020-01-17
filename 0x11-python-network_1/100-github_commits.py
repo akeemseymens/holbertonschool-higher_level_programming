ff77af2fe9d2ce19af78e4d0e0b1493b019d15c7
akeemseymens <812@holbertonschool.com>
2020-01-17 00:14:13 +0000

    added files

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Use the Github API to list the 10 most recent commits"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    for commit in r.json()[0:10]:
+        print('{}: {}'.format(commit.get('sha'), commit.get('commit')
+                              .get('author').get('name')))
--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Send a search request to the Star Wars API (swapi.co)"""
+"""
+Send a search request to the Star Wars API (swapi.co) and manage pagination
+"""
 
 if __name__ == '__main__':
     import requests
@@ -10,3 +12,8 @@ if __name__ == '__main__':
     print('Number of results: {}'.format(data.get('count')))
     for person in data.get('results'):
         print(person.get('name'))
+    while data.get('next'):
+        r = requests.get(data.get('next'))
+        data = r.json()
+        for person in data.get('results'):
+            print(person.get('name'))
