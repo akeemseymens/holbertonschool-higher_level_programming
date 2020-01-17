c5fbee334dbb1eec8c46c048e319c631df88984d
akeemseymens <812@holbertonschool.com>
2020-01-17 02:04:30 +0000

    added file

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Send a search request to the Star Wars API
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get('https://swapi.co/api/people/?search={}'
+                     .format(argv[1])).json()
+    print('Number of results: {}'.format(r.get('count')))
+    for person in r.get('results'):
+        print(person.get('name'))
+        for film in person.get('films'):
+            print('\t{}'.format(requests.get(film).json().get('title')))
+    while r.get('next'):
+        r = requests.get(r.get('next')).json()
+        for person in r.get('results'):
+            print(person.get('name'))
+            for film in person.get('films'):
+                print('\t{}'.format(requests.get(film).json().get('title')))
