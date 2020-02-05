6a5d8b88df9439742725b9f7f1a60e64617a4c14
akeemseymens <812@holbertonschool.com>
2020-02-05 07:00:13 +0000

    fixed syntax

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-function factorial (a) {
-  if (isNan(a) || a < 2) {
-    return 1;
-  }
-  return a * factorial(a - 1);
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
 }
-
 console.log(factorial(parseInt(process.argv[2])));
