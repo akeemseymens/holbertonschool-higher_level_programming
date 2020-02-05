01b5f15e1d1d1dba1165f4cdc4c7a2e6494162ba
akeemseymens <812@holbertonschool.com>
2020-02-05 01:42:10 +0000

    intial upload

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const lenArr = process.argv.length;
+if (lenArr <= 2) {
+  console.log('No argument');
+} else if (lenArr === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
