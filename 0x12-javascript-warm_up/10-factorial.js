7997c21e9c3e3dceba60846ccb861bc465d1a2f8
akeemseymens <812@holbertonschool.com>
2020-02-05 07:07:07 +0000

    change semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function factorial(n) {
-  return (n != 1) ? n * factorial(n - 1) : 1;
+function factorial (n) {
+  return (n !== 1 || isNaN(n)) ? n * factorial(n - 1) : 1;
 }
 console.log(factorial(parseInt(process.argv[2])));
