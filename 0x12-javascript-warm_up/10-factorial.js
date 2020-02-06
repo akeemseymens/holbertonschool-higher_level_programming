4ecb9006d240f9d229e35e4df76340d02642b64a
akeemseymens <812@holbertonschool.com>
2020-02-06 06:31:36 +0000

    added fils

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-function factorial (n) {
-  return (n !== 1 || isNaN(n)) ? n * factorial(n - 1) : 1;
+function factorial (a) {
+  if (!a || a < 2) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 }
+
 console.log(factorial(parseInt(process.argv[2])));
