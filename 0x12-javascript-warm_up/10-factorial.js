ff9674f8aea3c76278a4b4103295baea29906c2c
akeemseymens <812@holbertonschool.com>
2020-02-06 06:33:51 +0000

    added edge case

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,13 @@
-#!/usr/bin/node
-function factorial (a) {
-  if (!a || a < 2) {
-    return 1;
-  }
-  return a * factorial(a - 1);
-}
 
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  } else if (n === 0) {
+    return (1);
+  } else {
+    return (n * factorial(n - 1));
+  }
+}
 console.log(factorial(parseInt(process.argv[2])));
