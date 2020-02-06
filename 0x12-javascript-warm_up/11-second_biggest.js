5b7ffade855e3f03ad6f0655d5ed1f10685ac3cc
akeemseymens <812@holbertonschool.com>
2020-02-06 06:22:42 +0000

    added file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+let arr = [];
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  arr = (process.argv.slice(2));
+  const secLrg = arr.sort(function (a, b) { return a - b; })[arr.length - 2];
+  console.log(secLrg);
+}
