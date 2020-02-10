6988c392ca16d178343b0281bba02b57a273b3c1
akeemseymens <812@holbertonschool.com>
2020-02-10 22:09:06 +0000

    added files

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const square_inherit = require('./5-square');
+
+class Square extends square_inherit {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
