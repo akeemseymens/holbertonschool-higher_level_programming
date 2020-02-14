27c550cb6b39fc7700aef140a93aeda186cc9a69
akeemseymens <812@holbertonschool.com>
2020-02-14 06:57:32 +0000

    added scripts

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$.ajax({
+  url: 'https://swapi.co/api/films/?format=json',
+  type: 'GET',
+  dataType: 'json'
+}).done(function (json) {
+  const movies = json.results;
+  for (let i = 0; i < movies.length; i++) {
+    $('UL#list_movies').append('<li>' + movies[i].title + '</li>');
+  }
+});
